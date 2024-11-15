faeffcb2b423a80538b76c28771d33392ea0684a
Imad Karahodza <imadahmed.karahodza@elev.ga.ntig.se>
2024-11-15 11:16:08 +0100

    dasdas

--- Steg1/Program.cs
+++ Steg1/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Första
+
+// if (6 >= 3)
+// {
+//     Console.WriteLine("Hello world!");
+//     Console.ReadLine();
+// }
+
+
+
+//Andra
+
+// string userName = "";
+// Console.WriteLine("Hello user, type in your username to log in.");
+// Console.Write(">");
+// userName = Console.ReadLine();
+// if (userName == "kalleanka")
+// {
+//     Console.WriteLine("Welcome!");
+// }
+// Console.ReadLine();
+
+
+
+//Tredje
+
+// string userName = "";
+// string passWord = "";
+
+// Console.WriteLine("Hello user, type in your username and password to log in.");
+// Console.Write(">");
+// userName = Console.ReadLine();
+// Console.Write(">");
+// passWord = Console.ReadLine();
+
+// if (userName == "kalleanka" && passWord == "12345")
+// {
+//     Console.WriteLine("Welcome!");
+//     Console.ReadLine();
+// }
+
+
+// else
+// {
+//     Console.WriteLine("Wrong username or password.");
+//     Console.ReadLine();
+// 
+
+
+
+//Fjärde
+
+// for (int i = 0; i < 32; i++)
+// {
+//     Console.WriteLine("Hello, world!");
+// }
+
+// Console.ReadLine();
+
+
+
+//Femte
+
+// string passWord = "";
+
+// Console.WriteLine("Please write in your password");
+// Console.Write(">");
+
+// while (passWord != "12345")
+// {
+//     passWord = Console.ReadLine();
+//     if (passWord != "12345")
+//     {
+//         Console.WriteLine("Wrong password, try again.");
+//     }
+// }
+
+// Console.WriteLine("Welcome!");
+// Console.ReadLine();
+
+
+
+//Sexte
+
